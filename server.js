0b5451f93263b8bc4d5d97f82894a15c8a94a144
Pratush Rai <pratushrai0309@gmail.com>
2024-04-30 19:39:16 +0530

    Complete Backend

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const { getImageRaw, getImageMapped } = require('./storage.js');
+
+const Port = process.env.PORT || 8080;
+
+const app = express()
+const publicPath = path.join(__dirname, "/public");
+const droneUI = path.join(__dirname, "/public/droneUI.html");
+const homePage = path.join(__dirname, "/public/about.html");
+const assets = path.join(__dirname, "/Assets")
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(droneUI);
+});
+
+app.get('/images', async (req, res) => {
+  const rawImage = await getImageRaw();
+  const mappedImage = await getImageMapped();
+  res.send({ 
+    mappedURL : mappedImage,
+    rawURL: rawImage
+   })
+})
+
+app.listen(Port, () => {
+  console.log(`The app is running at http://localhost:${Port}`);
+})
