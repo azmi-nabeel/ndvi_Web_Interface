bad879e4477e50569464ceb92626cb75c99836e9
Pratush Rai <pratushrai0309@gmail.com>
2024-05-03 04:34:04 +0530

    Added Caching layer for weather API

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Dharwad&appid=${process.env.API_KEY}&units=metric`
 const getWeather = () => {
@@ -8,6 +9,15 @@ const getWeather = () => {
         if(data.error) {
           reject("Unable to fetch")
         }else{
+          const date = new Date();
+          const file = fs.writeFileSync('./cache/weather.json', JSON.stringify({
+            temp: data.main.temp,
+            temp_min: data.main.temp_min,
+            temp_max: data.main.temp_max,
+            humidity: data.main.humidity,
+            wind_speed: data.wind.speed,
+            time: date.getHours().toString() + ":" + date.getMinutes().toString(),
+          }));
           resolve({ 
             temp: data.main.temp,
             temp_min: data.main.temp_min,
